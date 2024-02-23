ced93304ce0585f14e1b47118ee2b3e2662dbe65
João Alberto <hey@joaoalberto.dev>
2024-02-22 23:25:12 -0300

    feat: basic REPL

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"monkey.lang/repl"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	fmt.Printf("Monkey.lang REPL\n")
+	fmt.Printf("Type your commands:\n")
+
+	repl.Start(os.Stdin, os.Stdout)
 }
